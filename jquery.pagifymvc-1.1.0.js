d3ab032e9ceb341f164150a60dbf72cedb393d00
Turhan Coşkun <turhancoskun@gmail.com>
2013-11-10 14:43:27 +0200

    Bug fixed for the default sorting element

--- jquery.pagifymvc-1.1.0.js
+++ jquery.pagifymvc-1.1.0.js
@@ -27,7 +27,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
                 beforeSend: function () {
                 },
                 pageSizeElement: $('#pagelength'),
-                sortingClass : 'sortings'
+                sortingClass : 'sorting'
             };
             options = $.extend({}, defaults, options);
             var obj = $(this)[0];
